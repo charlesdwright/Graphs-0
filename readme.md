953aca13349f3c1f5ba50a311e74d40db4aa28ea
charlesdwright <71340308+charlesdwright@users.noreply.github.com>
2021-05-11 18:58:05 -0400

    Initial commit.

---
+++ readme.md
@@ -0,0 +1,8 @@
+**Graph-0.**
+Web service takes text input in the format shown and returns a JSON tree structure.
+
+**Example:**
+`http://localhost:8080/graph/getjson/A,B;B,C;A,C;B,D;C,D;D,E;F,G;G,H;F,H`
+`http://localhost:8080/graph/getjson/apple,pear;apple,orange;orange,egg;orange,banana;apple,grapefruit`
+
+More later.
---
+++ src/main/java/com/example/springboottest/SpringBootGraphs0Application.java
@@ -0,0 +1,13 @@
+package com.example.springboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGraphs0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGraphs0Application.class, args);
+    }
+
+}
